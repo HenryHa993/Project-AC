b96071884eb338c49b202169625fc784e0150754
Henry Ha <henryha993@gmail.com>
2022-11-20 11:21:50 +0000

    Added simple lighting

--- Assets/Scripts/FieldOfView/FieldOfView.cs
+++ Assets/Scripts/FieldOfView/FieldOfView.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class FieldOfView : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
-    }
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector3[] vertices = new Vector3[3];
+        Vector2[] uv = new Vector2[3];
+        int[] triangles = new int[3];
+
+        vertices[0] = Vector3.zero;
+        vertices[1] = new Vector3(5,0);
+        vertices[2] = new Vector3(0,-5);
+
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
     }
 }
